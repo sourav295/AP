2d6ec9f0b81fa99d260696790a77c259f3443a7f
spanda2 <spanda2@SRH704-8S>
2017-01-17 22:36:18 +0800

    354

--- 354/354.cpp
+++ 354/354.cpp
@@ -2,6 +2,8 @@
 //
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <iostream>
+using namespace std;
 
 
 int main()
@@ -22,7 +24,23 @@ int main()
 		//read 4 segments
 		scanf("%d %d %d %d", &cardSegments[0], &cardSegments[1], &cardSegments[2], &cardSegments[3]);
 
-		
+		//construct int array of the segments
+		for (int j = 0; j < cardNumberSeg; j++){
+			int cardSegmentValue = cardSegments[j];
+			for (int k = 0; k < 4; k++){
+				creditCardNumber[((j + 1)*4 ) - ( k + 1 )] = cardSegmentValue % 10;
+				cardSegmentValue /= 10;
+			}
+		}
+		/*
+		for (int j = 0; j < cardNumberLen; j++) {
+			cout << creditCardNumber[j];
+		}
+		*/
+
+
+
+
 				
 	}
 
