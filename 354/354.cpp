237dda5c0dd7f92dd9160f83763de71d8dc2cd77
spanda2 <spanda2@SRH704-8S>
2017-01-17 22:12:26 +0800

    111

--- 354/354.cpp
+++ 354/354.cpp
@@ -22,27 +22,7 @@ int main()
 		//read 4 segments
 		scanf("%d %d %d %d", &cardSegments[0], &cardSegments[1], &cardSegments[2], &cardSegments[3]);
 
-		int cardNumberPos = 0;
-
-		//Iterate over segments to create card number array
-		for (int j = 0; j < cardNumberSeg; j++)
-		{
-			int card_segment = cardSegments[j];
-			
-			do {
-				int digit = card_segment % 10;
-				//populate card number to operate on
-				creditCardNumber[cardNumberPos] = digit;
-				cardNumberPos++;
-
-				card_segment /= 10;
-			} while (card_segment > 0);
-		}
-
-		for (int j = 0; j < cardNumberLen; j++)
-		{
-			printf("%d", creditCardNumber[j]);
-		}
+		
 				
 	}
 
