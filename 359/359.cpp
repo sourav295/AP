86f6b2f3d8fe376fad173816d4402148983754ec
spanda2 <spanda2@SRH704-8S>
2017-01-27 09:47:58 +0800

    init

---
+++ 359/359.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+//#include <vector>
+#include <bitset>
+#include <queue> 
+using namespace std;
+
+int main()
+{
+
+	bitset<8> baz(string("00000000"));
+	cout << baz;
+
+    return 0;
+}
+
