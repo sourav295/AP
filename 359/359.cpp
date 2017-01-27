a03c1a964d661a4a9ff56916c687fdae8b14d97b
spanda2 <spanda2@SRH704-8S>
2017-01-27 14:54:38 +0800

    fix split

--- 359/359.cpp
+++ 359/359.cpp
@@ -6,11 +6,81 @@
 #include <queue> 
 using namespace std;
 
+struct MemorySegment{
+
+	bitset<3> instruction;
+	bitset<5> content;
+
+	MemorySegment(uint8_t segment) {
+		
+				
+		//extract content
+		uint8_t count = 0;
+		for (segment; count < 5; segment >> 1){
+			content[count++] = segment & 1;
+			cout << (segment & 1) << "\n";
+		}
+		count = 0;
+		for (segment; count < 3; segment >> 1) {
+			instruction[count++] = segment & 1;
+			cout << (segment & 1) << "\n";
+		}
+
+		cout << instruction[2] << instruction[1] << instruction[0];
+
+	}
+	MemorySegment() {
+
+	}
+
+
+};
+
+
+struct Memory{
+
+	MemorySegment memorySegment[32];
+	uint8_t len;
+
+	uint8_t counter;
+	uint8_t acc;
+	
+	Memory(queue<string> input) {
+		
+		counter = 0;
+		acc = 0;
+
+		uint8_t count = 0;
+		for (input; !input.empty(); input.pop())
+			//memorySegment[count++] = MemorySegment(stoi(input.front(), nullptr, 2));
+		len = count;
+	};
+
+	/*
+	void display() {
+		cout << "\n";
+		for (uint8_t i = 0; i < len; i++)
+			cout << unsigned(memorySegment[i])<<", ";
+	}
+	*/
+	
+
+};
+
+
 int main()
 {
+	//queue<string> x;
+	//x.push("00000000");
+	//x.push("00000001");
+	//Memory m(x);
+	//m.display();
 
-	bitset<8> baz(string("00000000"));
-	cout << baz;
+	
+	MemorySegment x = MemorySegment((stoi("11111110", nullptr, 2)));
+	//cout<< x.instruction[0];
+
+
 
     return 0;
 }
