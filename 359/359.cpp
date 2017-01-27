02a567256fac65b5ce951b3cca8aba45617ba6ab
spanda2 <spanda2@SRH704-8S>
2017-01-28 00:11:29 +0800

    resolve cases

--- 359/359.cpp
+++ 359/359.cpp
@@ -8,28 +8,30 @@ using namespace std;
 
 struct MemorySegment{
 
-	bitset<3> instruction;
-	bitset<5> content;
+	uint8_t instruction;
+	uint8_t content;
 
 	MemorySegment(uint8_t segment) {
+		
+		instruction = 0;
+		content = 0;
+		
 		//extract content
 		uint8_t count = 0;
 		for (segment; count < 5; segment = segment >> 1){
-			content[count++] = segment & 1;
+			//content[count++] = segment & 1;
+			if (segment & 1)
+				content += (1 << count);
+			count++;
 		}
+		
 		count = 0;
 		for (segment; count < 3; segment = segment >> 1) {
-			instruction[count++] = segment & 1;
+			//instruction[count++] = segment & 1;
+			if (segment & 1)
+				instruction += (1 << count);
+			count++;
 		}
-	}
-
-	uint8_t getContent() {
-		uint8_t con = 0;
-		for (int i = 0; i < 5; i++) {
-			if (content[i])
-				con += (1 << i);
-		}
-		return con
 	}
 	
 	MemorySegment() {
@@ -55,33 +57,65 @@ struct Memory{
 
 		uint8_t count = 0;
 		for (input; !input.empty(); input.pop())
-			//memorySegment[count++] = MemorySegment(stoi(input.front(), nullptr, 2));
+			memorySegment[count++] = MemorySegment(stoi(input.front(), nullptr, 2));
 		len = count;
 	};
 
-	/*
+
 	void display() {
 		cout << "\n";
 		for (uint8_t i = 0; i < len; i++)
-			cout << unsigned(memorySegment[i])<<", ";
+			cout << unsigned(memorySegment[i].instruction)<<" " << unsigned(memorySegment[i].content) << " \n";
 	}
-	*/
 	
+	void interpretInstruction(uint8_t index) {
+
+		uint8_t instr = memorySegment[index].instruction;
+		
+		switch (unsigned(instr))
+		{
+		case 0:
+			cout << "0";
+			break;
+		case 1:
+			cout << "1";
+			break;
+		case 2:
+			cout << "2";
+			break;
+		case 3:
+			cout << "3";
+			break;
+		case 4:
+			cout << "4";
+			break;
+		case 5:
+			cout << "5";
+			break;
+		case 6:
+			cout << "6";
+			break;
+		case 7:
+			cout << "7";
+			break;
+		default:
+			break;
+		}
+
+	}
 
 };
 
 int main()
 {
-	//queue<string> x;
-	//x.push("00000000");
-	//x.push("00000001");
-	//Memory m(x);
-	//m.display();
+	queue<string> x;
+	x.push("01100000");
+	x.push("00000001");
+	Memory m(x);
+	m.display();
 
 	
-	MemorySegment x (stoi("11011111", nullptr, 2));
-	cout <<"contetn"<< (uint8_t)x.getContent();
-	//cout<< x.instruction[0];
+	m.interpretInstruction(0);
 
 	
 
