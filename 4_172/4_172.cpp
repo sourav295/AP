90f2af20176602856b71cd42eeb7b9c675cf80a7
spanda2 <spanda2@SRH703-8S>
2017-02-12 00:33:59 +0800

    error in project

--- 4_172/4_172.cpp
+++ 4_172/4_172.cpp
@@ -7,6 +7,7 @@
 #include<string>
 #include <set>
 #include <algorithm>
+#include <limits>
 using namespace std;
 
 typedef struct Junction Junction;
@@ -28,9 +29,9 @@ struct Junction {
 	int id;
 	int distance_src;
 	
-	Junction(int identity, int limit) {
+	Junction(int identity) {
 		id = identity;
-		distance_src = limit;
+		distance_src = numeric_limits<int>::max();
 	}
 
 	Junction(){}
@@ -45,12 +46,15 @@ bool sortHelper(Junction i, Junction j) { return (i.distance_src > j.distance_sr
 int main()
 {
 	int n,r;
+	int test_number = 0;
 	while (cin >> n >> r) {
+		test_number++;
+		
 		vector<Junction> priority_queue;
 
 		//create junctions 
 		for (int i = 0; i < n; i++) {
-			priority_queue.push_back(Junction(i, n));
+			priority_queue.push_back(Junction(i));
 		}
 		
 		//link the junctions 
@@ -71,53 +75,49 @@ int main()
 		//init src
 		priority_queue[0].distance_src = 0;
 
+		/*
 		while (!priority_queue.empty()) {
 			cout << "\n" << priority_queue.back().id;
 			queue<Link> links = priority_queue.back().neighbours;
 
 			
 			for (links; !links.empty(); links.pop())
-				cout << ",  " << (*(links.front().neightbour)).id;
+				cout << ",  " << (*(links.front().neightbour)).id<< " " << links.front().distance ;
 			priority_queue.pop_back();
-		}
+		}*/
 				
 		//djikstra
-		/*
 		int results = 0;
 		while (!priority_queue.empty()) {
 			sort(priority_queue.begin(), priority_queue.end(), sortHelper);
 
-			int curr_distance = priority_queue.back().distance_src;
-			queue<Link> extracted_links = priority_queue.back().neighbours;
-			
-			cout << "\n" << priority_queue.back().id << "  ";
+			Junction *junction = &(priority_queue.back());
 
-			if (priority_queue.back().id == (n - 1)) {
-				results = priority_queue.back().distance_src;
+			int curr_distance = (*junction).distance_src;
+			queue<Link> extracted_links = (*junction).neighbours;
+			
+			if ((*junction).id == (n - 1)) {
+				results = (*junction).distance_src;
 				break;
 			}
 
 			priority_queue.pop_back();
 
 			for (extracted_links; !extracted_links.empty(); extracted_links.pop()) {
-				Junction *j = extracted_links.front().neightbour;
-				int distance = extracted_links.front().distance;//distance till the neightbour
-
+				Link link = extracted_links.front();
+				Junction *j = link.neightbour;
+				int distance = link.distance;//distance till the neightbour
+				
 				int potential_distance = curr_distance + distance;
-				cout << (*j).id<< ", ";
-
-				if ((*j).distance_src < potential_distance)
+				//cout << (*j).id<< " P"<< potential_distance<< " "<< ", ";
+				if ((*j).distance_src > potential_distance)
 					(*j).distance_src = potential_distance;
 
 			}
 		}
 		
-		cout << "!"<<results;
-
-		*/
-
-
-
+		cout << "Set #" << test_number<<"\n";
+		cout <<results<<"\n";
 	}
 	
 	return 0;
