fda0df2846d26288b1edb5db2623ea5d957f045c
spanda2 <spanda2@SRH704-8S>
2017-02-18 01:52:45 +0800

    implement dijkstra

--- 4_207/4_207.cpp
+++ 4_207/4_207.cpp
@@ -26,40 +26,68 @@ struct Place {
 
 int main()
 {
-
+	
 	int n;
 	cin >> n;
 	string place;
+	getline(cin, place);//blank
+	
 	vector<Place> nodes;
 	vector<string> edges;
 	for (int i = 0; i < n; i++) {
 		getline(cin, place);
 		nodes.push_back(Place(place, i));
 	}
+	cout << "Done";
 
 	int n_roads;
 	cin >> n_roads;
+
+	getline(cin, place);//blank
+
 	for (int i = 0; i < n_roads; i++) {
 		string road;
 		getline(cin, road);
-		stringstream ss(road);
-		string p1, p2, d1, d2 = "";
-		getline(ss, p1, ':');
-		getline(ss, p2, ' ');
-		getline(ss, d1, ' ');
 		
-		if (getline(ss, d2)) {
-			for (int j = 0; j < edges.size(); j++) {
-				if(!)
+		int indexOfDigit = -1;
+		for (int j = 0; j < road.length(); j++)
+		{
+			if (isdigit(road.at(j)))
+			{
+				indexOfDigit = j;
+				break;
 			}
 		}
+		string places = road.substr(0, indexOfDigit - 1);
+		string num = road.substr(indexOfDigit, road.length() - indexOfDigit);
+		
+		stringstream ss_places(places);
+		stringstream ss_nums(num);
+		string p1, p2 = "";
+		int d1, d2;
+
+		getline(ss_places, p1, ':');
+		getline(ss_places, p2);
+		
+		int index_p1, index_p2 = 0;
+		for (int j = 0; j < n; j++) {
+			if (!p1.compare(nodes[j].name))
+				index_p1 = j;
+			if (!p2.compare(nodes[j].name))
+				index_p2 = j;
+		}
+		
+		
+		ss_nums >> d1;
+		
+		if (ss_nums >> d2) {
+			//bi-directional
+			nodes[index_p2].id_dist.push({ index_p1, d2 });
+		}
+		nodes[index_p1].id_dist.push({index_p2, d1});
+
 	}
 
-
-
-
-
-	
 
 
     return 0;
