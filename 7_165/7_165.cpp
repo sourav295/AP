54578beafd48e691264dc10aad51acb2f2ad1cf6
spanda2 <spanda2@SRH705-8S>
2017-03-07 01:42:15 +0800

    fgdfdg

--- 7_165/7_165.cpp
+++ 7_165/7_165.cpp
@@ -16,61 +16,11 @@ using namespace std;
 
 
 const int n_limit = 1000;
-const int m_limit = 49500;
-int A[n_limit];
+
 
 int X[n_limit], Y[n_limit];
 int graph[n_limit][n_limit];
 
-
-struct Edge {
-
-	int indexA, indexB;
-	unsigned int ppa;
-
-	Edge(int a, int b, unsigned int dist) {
-		indexA = a;
-		indexB = b;
-		ppa = dist;
-	}
-
-	Edge() {}
-
-}edges[m_limit];
-
-int compareEdge(const void * a, const void * b)
-{
-	if ((*(Edge*)a).ppa > (*(Edge*)b).ppa)
-		return +1;
-	else
-		return -1;
-}
-
-
-void Union(int element1, int element2);
-void UnionSet(int set1, int set2);
-int Find(int element);
-
-int Find(int element) {
-	if (A[element] < 0)
-		return element;
-	else
-		return Find(A[element]);
-}
-
-void Union(int element1, int element2) {
-	int root1 = Find(element1);
-	int root2 = Find(element2);
-	if (A[root1] < A[root2])
-		UnionSet(root1, root2);
-	else
-		UnionSet(root2, root1);
-}
-
-void UnionSet(int set1, int set2) {
-	A[set1] += A[set2];
-	A[set2] = set1;
-}
 
 int minDistance(int dist[], bool sptSet[], int n)
 {
@@ -88,7 +38,6 @@ int minDistance(int dist[], bool sptSet[], int n)
 int dijkstra(int n)
 {
 	int dist[n_limit];
-	int parent[n_limit];
 	bool sptSet[n_limit];
 	for (int i = 0; i < n; i++){
 		dist[i] = numeric_limits<int>::max();
@@ -102,26 +51,21 @@ int dijkstra(int n)
 		sptSet[u] = true;
 
 		if (u == 1) {
-			//complete
-			int max = numeric_limits<int>::min();
-			int cur = u;
-			int par = parent[u];
-			while (0 != cur) {
-				if (dist[cur] - dist[par] > max)
-					max = dist[cur] - dist[par];
-				cur = par;
-				par = parent[cur];
-			}
-			
-			return max;
+			return dist[u];
 		}
 			
 
 		// Update dist value of the adjacent vertices of the picked vertex.
 		for (int v = 0; v < n; v++)
-			if (!sptSet[v] && graph[u][v] && dist[u] != numeric_limits<int>::max() && dist[u] + graph[u][v] < dist[v]) {
-				dist[v] = dist[u] + graph[u][v];
-				parent[v] = u;
+			if (!sptSet[v] && graph[u][v] && dist[u] != numeric_limits<int>::max()) {
+				int max_fromIncoming = graph[u][v] > dist[u] ? graph[u][v] : dist[u];
+				
+				if (dist[v] == numeric_limits<int>::max()) {
+					dist[v] = max_fromIncoming;
+				}
+				else if (max_fromIncoming < dist[v]) {
+					dist[v] = max_fromIncoming;
+				}
 			}
 	}
 
@@ -154,50 +98,18 @@ int main()
 
 		int count = 0;
 		for (int i = 0; i < N; i++)
-			for (int j = i + 1; j < N; j++)
-				edges[count++] = Edge(i, j, pow(X[i] - X[j], 2) + pow(Y[i] - Y[j], 2));
-
-		for (int i = 0; i < N; i++)
-			A[i] = -1;
-
-		qsort(edges, count, sizeof(Edge), compareEdge);
-
-		for (int i = 0; i < count; i++) {
-			Edge e = edges[i];
-			if (Find(e.indexA) != Find(e.indexB)) {
-				Union(e.indexA, e.indexB);
+			for (int j = i + 1; j < N; j++) {
+				int d = pow(X[i] - X[j], 2) + pow(Y[i] - Y[j], 2);
+				graph[i][j] = d;
+				graph[j][i] = d;
 			}
+				//edges[count++] = Edge(i, j, pow(X[i] - X[j], 2) + pow(Y[i] - Y[j], 2));
 
-			//search complete
-			if (Find(0) == Find(1)) {
-				int root = Find(0);
-				int hop_count = 0;
-				
-				for (int j = 0; j < N; j++)
-					for (int k = 0; k < N; k++)
-						graph[j][k] = 0;
+		if (test > 1)
+			cout << "\n";
 
-				for (int j = 0; j <= i; j++) {
-					int root = Find(0);
-					
-					Edge e = edges[j];
-					if (root == Find(j)) {
-						graph[e.indexA][e.indexB] = e.ppa;
-						graph[e.indexB][e.indexA] = e.ppa;
-					}
-				}
-
-				if (test > 1)
-					cout << "\n";
-
-				cout << "Scenario #" << test++ << "\n";
-				printf("Frog Distance = %.3f\n", sqrt(dijkstra(N)));
-
-				break;
-			}
-
-
-		}
+		cout << "Scenario #" << test++ << "\n";
+		printf("Frog Distance = %.3f\n", sqrt(dijkstra(N)));
 
 	}
 
---
+++ 7_165/backup.h
@@ -0,0 +1,8 @@
+#pragma once
+class backup
+{
+public:
+	backup();
+	~backup();
+};
+
