213c3c0192fc0e9becb18196139ef0968fa708bb
spanda2 <spanda2@SRH701-8S>
2017-04-04 01:08:24 +0800

    rdt

--- 548/548.cpp
+++ 548/548.cpp
@@ -14,21 +14,41 @@ const int n_limit = 64;
 bool startNumber[n_limit];
 bool endNumber[n_limit];
 
-void loadDest(long src, long dest) {
+void loadDest(long dest) {
+	int i;
+	for (i = 0; i < n_limit; ++i) {  // assuming a 32 bit int
+		endNumber[i]   = dest & (1 << i) ? 1 : 0;
+	}
+}
+
+void loadSrc(long src) {
 	int i;
 
 
 	for (i = 0; i < n_limit; ++i) {  // assuming a 32 bit int
 		startNumber[i] = src & (1 << i) ? 1 : 0;
-		endNumber[i]   = dest & (1 << i) ? 1 : 0;
 	}
 }
 
 
 int main()
 {
-	storeInArray(7, 8);
+	loadDest(8);
+	loadSrc(7);
 	cout << startNumber[0];
+
+	int sig_bit = n_limit - 1;
+
+	for (int i = sig_bit; i >= 0; i--) {
+		if (startNumber[i] == true) {
+			sig_bit = i;
+			break;
+		}
+	}
+
+
+
+
 
 	int x = 10;
 	/*
