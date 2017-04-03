1a35125ce4c6c75ac40f0f10558dbcb242522e3d
spanda2 <spanda2@SRH701-8S>
2017-04-03 19:08:22 +0800

    finish it later

--- 548/548.cpp
+++ 548/548.cpp
@@ -9,9 +9,29 @@
 #include <stdint.h>
 using namespace std;
 
+const int n_limit = 64;
+
+bool startNumber[n_limit];
+bool endNumber[n_limit];
+
+void loadDest(long src, long dest) {
+	int i;
+
+
+	for (i = 0; i < n_limit; ++i) {  // assuming a 32 bit int
+		startNumber[i] = src & (1 << i) ? 1 : 0;
+		endNumber[i]   = dest & (1 << i) ? 1 : 0;
+	}
+}
+
+
 int main()
 {
-	
+	storeInArray(7, 8);
+	cout << startNumber[0];
+
+	int x = 10;
+	/*
 	uint64_t a, b, c, d;
 	while (cin >> a >> b) {
 		
@@ -26,7 +46,7 @@ int main()
 		printf("%" PRIu64 "\n", (d | b));
 		//printf("%llu\n", );
 	}
-	
+	*/
     return 0;
 }
 
