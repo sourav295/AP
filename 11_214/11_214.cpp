d499b3dd52c8155ed9d86b585e5394a75e754c31
spanda2 <spanda2@SRH701-8S>
2017-04-09 17:20:22 +0800

    done

---
+++ 11_214/11_214.cpp
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <queue> 
+#include<string>
+#include <stack>
+#include <limits>
+#include <sstream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+bool isPrime(unsigned long int x) {
+	unsigned long int x_half = x / 2;
+	bool isPreime = true;
+	
+	for (int i = 2; i < x_half; i++)
+		if (x%i == 0)
+			return false;
+	return true;
+}
+
+
+int main()
+{
+	unsigned long int x, y, lower_prime, upper_prime;
+	while (cin >> x) {
+		if (x == 0)
+			break;
+		if (isPrime(x)) {
+			cout << 0 << "\n";
+		}
+		else {
+			
+			for (y = x; true; y++) {
+				if (isPrime(y)){
+					upper_prime = y;
+					break;
+				}
+			}
+			for (y = x; true; y--) {
+				if (isPrime(y)) {
+					lower_prime = y;
+					break;
+				}
+			}
+			cout << upper_prime - lower_prime << "\n";
+		}
+
+
+
+	}
+
+
+    return 0;
+}
+
