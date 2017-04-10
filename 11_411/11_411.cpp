72bfefbefa7081408dfe01645ef05ece9606856a
spanda2 <spanda2@SRH701-8S>
2017-04-10 20:01:31 +0800

    fix small error

--- 11_411/11_411.cpp
+++ 11_411/11_411.cpp
@@ -14,37 +14,76 @@ using namespace std;
 
 
 const int c_limit = 47;
+//const int n_limit = 1000000000;
 vector<pair<int, int>> researved(c_limit);
+vector<int> rowBeingJudged(c_limit + 2);
+int possibilities;
 
+int calc_possibilities(int start, int end) {
 
+	if (end - start >= 3)
+		return end - start - 2;//minus 1 to find the gaps, another minus one to find the combinations
+	else
+		return 0;
+}
 
 
 int main()
 {
-	int c = 10;
-	int input, emptySeat_streak;
-	for (int i = 0; i < c; i++) {
-		cin >> input;
-		researved[i] = { input, 0 };
-	}
-	for (int i = 0; i < c; i++) {
-		cin >> input;
-		researved[i] = { researved[i].first, input };
-	}
-	sort(researved.begin(), researved.begin() + c);
+	int n_test;
+	int n, m, c;
+	cin >> n_test;
+	for (int test = 0; test < n_test; test++) {
 
-	int previous_row = 0;
-	for (int i = 0; i < c; i++) {
-				
-		if (researved[i].first > previous_row) {
+		cin >> n >> m >> c;
+		possibilities = 0;
+
+		int input, n_uniqueRows;
+		for (int i = 0; i < c; i++) {
+			cin >> input;
+			researved[i] = { input, 0 };
+		}
+		for (int i = 0; i < c; i++) {
+			cin >> input;
+			researved[i] = { researved[i].first, input };
+		}
+
+		sort(researved.begin(), researved.begin() + c);
+
+		int i = 0;
+		int j = 0;
+		int current_row = 0;
+		n_uniqueRows = 0;
+
+		possibilities = 0;
+		while (i < c) {
 			//new row
-			emptySeat_streak =  
-		}
-		else {
+			n_uniqueRows++;
+			current_row = researved[i].first;
 
+			j = 0;
+			rowBeingJudged[j++] = 0;
+			while (true) {
+				rowBeingJudged[j++] = researved[i++].second;
+				if (researved[i].first != current_row) {
+					rowBeingJudged[j++] = m + 1;
+					break;
+				}
+			}
+			for (int k = 0; k < j - 1; k++) {
+				possibilities += calc_possibilities(rowBeingJudged[k], rowBeingJudged[k + 1]);
+			}
 		}
+		possibilities += (m - 1)*(n - n_uniqueRows);
+
+		cout << "Case #"<<test<<": "<<possibilities<<"\n";
+
+
+
 	}
 
+
+	
     return 0;
 }
 
