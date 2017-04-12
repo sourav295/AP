d10c4cdaaea3862b275062a8ca33f3b7856d8992
sourav295 <sourav295@gmail.com>
2017-04-12 13:33:56 +0800

    time limit exceed

--- 11_462/11_462.cpp
+++ 11_462/11_462.cpp
@@ -10,14 +10,25 @@
 #include <sstream>
 #include <algorithm>
 #include <iomanip>
+#include <bitset>
 using namespace std;
 
 const long n_limit = 100000000 + 1;
-bool  flag[n_limit + 1];
-long  prime[n_limit + 1];
+const long bit_size = 64;
+const long bins = (n_limit/bit_size) + 1;
 
-long count_prime;
+bitset<bit_size> flag[bins];
 long primeNumbers_calculated;
+
+long count_prime = 0;
+
+void updateFlag(long pos, bool value) {
+	(flag[(pos / bit_size)])[(pos % bit_size)] = value;
+}
+
+bool getFlag(long pos) {
+	return (flag[(pos / bit_size)])[(pos % bit_size)];
+}
 
 
 void getPrime(long n) {
@@ -26,66 +37,56 @@ void getPrime(long n) {
 		return;
 	else
 		primeNumbers_calculated = n;//new value
-
-
+	
+	for (long i = 0; i <= n; i++) {
+		updateFlag(i, true);
+	}
 
 	for (long i = 2; i <= n; i++) {
-
-		if (flag[i] == true) {
-			prime[count_prime++] = i;
+		if (getFlag(i)) {
+			count_prime++;
 		}
-
-		for (long j = 0; j<count_prime && i*prime[j] <= n; j++) {
-			flag[i*prime[j]] = false;
-			if (i%prime[j] == 0)
+		long k = 0;//number of prime numbers utilized in calculation 2..count
+		
+		for (long j = 2; i*j <= n; j++) {
+			if (!getFlag(j))
+				continue;
+			//j is a prime number
+			k++;
+			updateFlag(i*j, false);
+			if (i%j == 0 || k == count_prime)
 				break;
 		}
 	}
+	updateFlag(1, true);
 }
 
 int main()
 {
-	primeNumbers_calculated = 0;
-	count_prime = 0;
-	for (long i = 0; i <= n_limit; i++) {
-		flag[i] = true;
-	}
 
-	queue<long> input;
+	
+	primeNumbers_calculated = 0;
+	
+
 	long in;
 	long half_input, best_p1;
 	
-	long p1, p2;
 	while (cin >> in) {
 		getPrime(in);
 		half_input = in / 2;
 
-
-		best_p1 = -1;
-		//check special case, p1 = 1
-		if (flag[in - 1]) {
-			best_p1 = 1;
-		}
-
-		for (long i = 0; prime[i] <= half_input; i++) {
-			
-			p1 = prime[i];
-			p2 = in - prime[i];
-			if (flag[p2]) {
-				//checks if p2 is indeed a prime number
-				best_p1 = p1;
+		bool found = false;
+		for (long i = half_input; i >= 1; i--) {
+			if (getFlag(i) && getFlag(in - i)) {
+				found = true;
+				cout << in << " is the sum of " << i << " and " << in - i<< ".\n";
+				break;
 			}
 		}
-		
-		if (best_p1 == -1) {
-			cout <<in<<" is not the sum of two primes!\n";
-		}
-		else {
-			cout <<in<<" is the sum of "<<best_p1<<" and "<<in - best_p1<<".\n";
-		}
+		if(!found)
+			cout << in << " is not the sum of two primes!\n";
 
 	}
-
 	
 	return 0;
 }
