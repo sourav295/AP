623a1b9364edfdb2df4a24fea1f6f585d38e7a8d
spanda2 <spanda2@SRH702-8S>
2017-04-13 16:28:30 +0800

    should be done

--- 548/548.cpp
+++ 548/548.cpp
@@ -19,32 +19,50 @@ using namespace std;
 
 
 
+unsigned long long getSeqOfOne(unsigned long long x, unsigned long long y) {
+	
+	//unsigned long long x = pow(10, 18);
+	int closestHigherPower2 = floor(log(x) / log(2)) + 1;
+	unsigned long long lower_lim = pow(2, closestHigherPower2 - 1);
+	lower_lim--;
+	if (lower_lim >= y) {
+		unsigned long long res = (pow(2, closestHigherPower2));
+		res--;
+		return res;
+	}
+	else {
+		return 0;
+	}
+	
+}
+
 unsigned long long getHighestSum(unsigned long long x, unsigned long long y) {
 	unsigned long long bitsToChange, x_copy, sum;
 	x_copy = x;
-	int highest_hideFlip = -1; 
+	int highest_hideFlip = -1;
 	bitsToChange = x ^ y;
-	for (int i = 0; bitsToChange != 0; bitsToChange >>= 1, x >>= 1, i++) {//shift towards left
-		bool change   = bitsToChange & 1;
+	for (int i = 0; bitsToChange != 0; bitsToChange >>= 1, x >>= 1, i++) {//shift towards left 
+		bool change = bitsToChange & 1;
 		bool hideFlip = change && (x & 1);
-
-		if (hideFlip){
+		if (hideFlip) {
 			highest_hideFlip = i;
-		}
-		
+		}	
 	}
 	
 	unsigned long long noChange_mask = 0;
-	if(highest_hideFlip > -1)
-		noChange_mask = (1 << (highest_hideFlip+1)) - 1;
-	//cout << noChange_mask << "\n";
-	sum =  (x_copy & noChange_mask) | (y & (~noChange_mask));
+	if (highest_hideFlip > -1)
+			noChange_mask = (1 << (highest_hideFlip + 1)) - 1;
+	//cout << noChange_mask << "\n"; 
+	sum = (x_copy & noChange_mask) | (y & (~noChange_mask));
 	return sum;
+	
 }
+
 
 
 int main()
 {
+	
 	
 	unsigned long long a, b, candidate1, candidate2;
 	string line;
@@ -56,18 +74,17 @@ int main()
 			cout << a << "\n";
 			continue;
 		}
-		int pos = floor(log(b) / log(2));
-		unsigned long long highest_precceding_multipleOf2 = (1 << pos);
+		
+		
 		candidate1 = 0;
 		candidate2 = 0;
-		if (highest_precceding_multipleOf2  > a) {
-			candidate1 =  2 * highest_precceding_multipleOf2 - 1;
-		}
+		
+		candidate1 = getSeqOfOne(b, a);
 		candidate2 = getHighestSum(b-1, b);
 		if (candidate1 > candidate2)
-			cout << candidate1 << "\n";
+			cout << setprecision(40)<<candidate1 << "\n";
 		else
-			cout << candidate2 << "\n";
+			cout << setprecision(40)<<candidate2 << "\n";
 
 	}
 	
