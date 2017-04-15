350c2536e74ba12d3ef072df0ce7cd905f11e948
spanda2 <spanda2@SRH702-8S>
2017-04-15 11:59:23 +0800

    should work

--- 12_209/12_209.cpp
+++ 12_209/12_209.cpp
@@ -15,18 +15,44 @@ using namespace std;
 const int m_limit = 100;
 bool isIsolated[m_limit];
 
-struct Point {
-	int x, y;
+struct Line{
+	int x1, y1, x2, y2;
 
-	int determinant(Point other_point) {
+	Line(int ax1, int ay1, int ax2, int ay2) {
+		x1 = ax1;
+		x2 = ax2;
+		y1 = ay1;
+		y2 = ay2;
+	}
+	Line() {}
 
+	int reltivePosition(Line other) {
+		int x3 = other.x1;
+		int x4 = other.x2;
+		int y3 = other.y1;
+		int y4 = other.y2;
+
+		int first_angle  = (x2*y3 - x3*y2) - x1*(y3 - y2) + y1*(x3 - x2);
+		int second_angle = (x2*y4 - x4*y2) - x1*(y4 - y2) + y1*(x4 - x2);
+
+		return first_angle * second_angle;
 	}
 
-};
+}all_lines[m_limit];
+
+bool isIntersecting(Line l1, Line l2) {
+	if (l1.reltivePosition(l2) <= 0 && l2.reltivePosition(l1) <= 0)
+		return true;
+	return false;
+}
+
 
 int main()
 {
 	int M, N;
+	int x1, y1, x2, y2;
+
+	
 	cin >> N;
 	for (int n = 0; n < N; n++) {
 		cin >> M;
@@ -37,11 +63,27 @@ int main()
 		}
 
 		for (int m = 0; m < M; m++) {
-			
+			cin >> x1 >> y1 >> x2 >> y2;
+			all_lines[m] = Line(x1, y1, x2, y2);
 		}
 
+		for (int i = 0; i < M; i++) {
+			for (int j = i + 1; j < M; j++) {
+				
+				if (isIntersecting(all_lines[i], all_lines[j])) {
+					isIsolated[i] = false;
+					isIsolated[j] = false;
+				}
+			}
+		}
+		int count = 0;
+		for (int i = 0; i < M; i++) {
+			if (isIsolated[i]) {
+				count++;
+			}
+		}
+		cout << count << "\n";
 	}
-
     return 0;
 }
 
