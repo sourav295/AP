66057212a3e9a32e2d1fd05f6729f44df411970a
spanda2 <spanda2@SRH702-8S>
2017-04-15 22:50:24 +0800

    better sol

--- 12_248/12_248.cpp
+++ 12_248/12_248.cpp
@@ -13,9 +13,8 @@
 using namespace std;
 
 
-const int rect_n_sides = 4;
 
-
+/*
 struct Point
 {
 	long long x;
@@ -81,6 +80,77 @@ bool doIntersect(Line a, Line b)
 
 	return false;
 }
+*/
+
+
+const int rect_n_sides = 4;
+float distance(int ax1, int ay1, int ax2, int ay2) {
+	float x_diffsq = pow(ax1 - ax2, 2);
+	float y_diffsq = pow(ay1 - ay2, 2);
+	return sqrt(x_diffsq + y_diffsq);
+}
+
+struct Line {
+	long long x1, y1, x2, y2;
+
+	float dist;
+
+	Line(int ax1, int ay1, int ax2, int ay2) {
+		x1 = ax1;
+		x2 = ax2;
+		y1 = ay1;
+		y2 = ay2;
+
+		dist = distance(x1, y1, x2, y2);
+	}
+	Line() {}
+
+	long long reltivePosition(Line other) {
+		long long x3 = other.x1;
+		long long x4 = other.x2;
+		long long y3 = other.y1;
+		long long y4 = other.y2;
+
+		long long first_angle = (x2*y3 - x3*y2) - x1*(y3 - y2) + y1*(x3 - x2);
+		long long second_angle = (x2*y4 - x4*y2) - x1*(y4 - y2) + y1*(x4 - x2);
+
+		return first_angle * second_angle;
+	}
+
+}rect[rect_n_sides];
+
+Line objective;
+
+float maxDistancebtwLines(Line l1, Line l2) {
+
+	vector<pair<int, int>> all_points;
+	all_points.push_back({ l1.x1, l1.y1 });
+	all_points.push_back({ l1.x2, l1.y2 });
+	all_points.push_back({ l2.x1, l2.y1 });
+	all_points.push_back({ l2.x2, l2.y2 });
+
+	sort(all_points.begin(), all_points.begin() + 4);
+
+	return distance(all_points[0].first, all_points[0].second, all_points[3].first, all_points[3].second);
+}
+
+
+
+bool isIntersecting(Line l1, Line l2) {
+
+	long long rel_pos1 = l1.reltivePosition(l2);
+	long long rel_pos2 = l2.reltivePosition(l1);
+	if ((rel_pos1 <= 0 && rel_pos2 <= 0) && !(rel_pos1 == 0 && rel_pos2 == 0))//one of them can be 0, not both
+		return true;
+
+	bool distance_ok = (maxDistancebtwLines(l1, l2) < (l1.dist + l2.dist));
+	//bool are_colinear = isColinear(l1, l2);
+
+	if (rel_pos1 == 0 && rel_pos2 == 0 && distance_ok) {
+		return true;
+	}
+	return false;
+}
 
 // Driver program to test above functions
 int main()
@@ -94,12 +164,12 @@ int main()
 		cin >> xstart >> ystart >> xend >> yend >> xleft >> ytop >> xright >> ybottom;
 
 
-		objective = Line(Point(xstart, ystart), Point(xend, yend));
+		objective = Line(xstart, ystart, xend, yend);
 
-		rect[0] = Line(Point(xleft, ytop), Point(xright, ytop));
-		rect[1] = Line(Point(xleft, ytop), Point(xleft, ybottom));
-		rect[2] = Line(Point(xright, ybottom), Point(xright, ytop));
-		rect[3] = Line(Point(xright, ybottom), Point(xleft, ybottom));
+		rect[0] = Line(xleft, ytop          , xright, ytop     );
+		rect[1] = Line(xleft, ytop          , xleft , ybottom  );
+		rect[2] = Line(xright, ybottom      , xright, ytop     );
+		rect[3] = Line(xright, ybottom      , xleft, ybottom   );
 
 		bool liesInside_1 = xstart >= xleft && ystart >= ybottom && xstart <= xright && ystart <= ytop;
 		bool liesInside_2 = xend >= xleft && yend >= ybottom && xend <= xright && yend <= ytop;
@@ -111,7 +181,7 @@ int main()
 		
 		bool intersect = false;
 		for (int i = 0; i < rect_n_sides; i++) {
-			if (doIntersect(objective, rect[i])) {
+			if (isIntersecting(objective, rect[i])) {
 				intersect = true;
 				break;
 			}
