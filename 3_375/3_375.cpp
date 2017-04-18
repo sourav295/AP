fff4cc9e18a62be021ef7cfc7c2850d0624ff6aa
spanda2 <spanda2@SRH702-8S>
2017-04-18 11:33:23 +0800

    change id function

---
+++ 3_375/3_375.cpp
@@ -0,0 +1,162 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+//#include <vector>
+#include <utility>
+#include <queue> 
+#include<string>
+#include <set>
+#include <algorithm>
+#include <sstream>
+#include <limits>
+#include <iomanip>
+using namespace std;
+
+
+const int max_cap = 25;
+const int n_bucks = 4;
+const int n = (max_cap +1) * n_bucks;
+
+bool ingredient[n][n];
+bool visited[n];
+int dist[n];
+bool sptSet[n];
+
+
+
+int cap[n_bucks];
+
+int T;
+
+
+int minDistance()
+{
+	// Initialize min value
+	int min = numeric_limits<int>::max();
+	int min_index;
+
+	for (int v = 0; v < n; v++)
+		if (sptSet[v] == false && dist[v] <= min) {
+			min = dist[v];
+			min_index = v;
+		}
+
+	return min_index;
+}
+
+
+
+
+int dijkstra(int src)
+{
+	for (int i = 0; i < n; i++) {
+		dist[i] = numeric_limits<int>::max();
+		sptSet[i] = false;
+	}
+
+	dist[src] = 0;
+
+	for (int count = 0; count < n; count++)
+	{
+		int u = minDistance();
+		sptSet[u] = true;
+
+		for (int v = 0; v < n; v++)
+			if (!sptSet[v] && ingredient[u][v] && dist[u] != numeric_limits<int>::max() && dist[u] + ingredient[u][v] < dist[v])
+				dist[v] = dist[u] + 1;
+	}
+
+	return 0;
+}
+
+void clearingredient() {
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			ingredient[i][j] = false;
+}
+
+
+int getId(vector<int> s) {
+	int sum = 0;
+	for (int i = 0; i < n_bucks; i++)
+		sum += (s[i]);
+	for (int i = 1; i < n_bucks; i++)
+		sum += (max_cap + 1);
+	
+	return sum;
+}
+
+bool can_fill(vector<int> s, int bucket) {
+	return s[bucket] < cap[bucket];
+}
+
+bool can_empty(vector<int> s, int bucket) {
+	return s[bucket] > 0;
+}
+
+int can_pour(vector<int> s, int from, int to) {
+	int measure = s[from] < (cap[to] - s[to]) ? s[from] : (cap[to] - s[to]);
+	return measure;
+}
+
+void transition(vector<int> s) {
+	int id = getId(s);
+	if (visited[id])
+		return;
+	visited[id] = true;
+
+	vector<int> temp;
+
+	for (int i = 0; i < n_bucks; i++) {
+		if (can_fill(s, i)) {
+			temp = s;
+			temp[i] = cap[i];
+			transition(temp);
+			ingredient[id][getId(temp)] = true;
+		}
+
+		if(can_empty(s, i)) {
+			temp = s;
+			temp[i] = 0;
+			transition(temp);
+			ingredient[id][getId(temp)] = true;
+		}
+	}
+
+	for(int i = 0; i < n_bucks; i++)
+		for (int j = 0; j < n_bucks; j++) {
+			if (i != j && can_pour(s, i, j) > 0) {
+				int m = can_pour(s, i, j);
+				
+				temp = s;
+				temp[i] -= m;
+				temp[j] += m;
+				
+				transition(temp);
+				ingredient[id][getId(temp)] = true;
+			}
+		}
+
+}
+
+int main() {
+	int id;
+	while (cin >> cap[0] >> cap[1] >> cap[2] >> cap[3] >> T) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++)
+				ingredient[i][j] = false;
+			visited[i] = false;
+		}
+
+		vector<int> state(n_bucks);
+
+		transition(state);
+		int y = 0;
+
+
+	}
+
+
+}
+
+
