4d7ae7c5a301e9c5975a64f8cbd87231ea2ae09e
spanda2 <spanda2@SRH702-8S>
2017-04-19 00:47:39 +0800

    do it in the morning

---
+++ 4_378/4_378.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <queue> 
+#include<string>
+#include <set>
+#include <algorithm>
+#include <limits>
+#include <sstream>
+using namespace std;
+
+
+int main()
+{
+	int M, N;
+	int P, L, X;
+	int T, V;
+
+	while (cin >> M >> N) {
+		while()
+	}
+
+
+    return 0;
+}
+
