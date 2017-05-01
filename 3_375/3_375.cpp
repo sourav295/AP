3d52606c6b4a6139c315504b0e0ba2dd17a29a9f
spanda2 <spanda2@SRH702-8S>
2017-05-01 15:57:56 +0800

    do later

--- 3_375/3_375.cpp
+++ 3_375/3_375.cpp
@@ -11,9 +11,183 @@
 #include <limits>
 #include <iomanip>
 #include <map>
+#include <array>
 using namespace std;
 
+const int n_bucks = 4;
+int T;
+array<int, 4> capacities = { 0, 0, 0, 0 };
 
+struct Node;
+void constructGraph(Node *n);
+
+map<array<int, 4>, Node*> all_states;
+vector<Node*> all_nodes;
+
+int minimumDistance = numeric_limits<int>::max();
+
+bool existInMap(array<int, 4> t) {
+	return !(all_states.count(t) == 0);
+}
+
+struct Node {
+	int state[n_bucks];
+	bool visited;
+	bool target = false;
+
+	vector<Node*> next;
+
+	int distance;
+
+	Node(array<int, 4> s) {
+		for (int i = 0; i < n_bucks; i++) {
+			state[i] = s[i];
+		}
+		distance = numeric_limits<int>::max();
+		visited = false;
+	}
+
+	bool can_fill(int bucket_id) {
+		return state[bucket_id] < capacities[bucket_id];
+	}
+
+	bool can_empty(int bucket_id) {
+		return state[bucket_id] > 0;
+	}
+
+	int can_pour( int from, int to) {
+		int measure = state[from] < (capacities[to] - state[to]) ? state[from] : (capacities[to] - state[to]);
+		return measure;
+	}
+
+	array<int, 4> copyState() {
+		array<int, 4> new_state = { 0, 0, 0 , 0 };
+		for (int i = 0; i < n_bucks; i++)
+			new_state[i] = state[i];
+		return new_state;
+	}
+
+	void addState(array<int, 4> new_state) {
+		if (!existInMap(new_state)) {
+			Node * newNode = new Node(new_state);
+			next.push_back(newNode);
+
+			all_states[new_state] = newNode;
+			all_nodes.push_back(newNode);
+
+			for (int i = 0; i < n_bucks; i++)
+				if (new_state[i] == T) {
+					newNode->target = true;
+					cout << "T";
+				}
+
+			if(!(newNode->target))
+				constructGraph(newNode);
+		}
+		next.push_back(all_states[new_state]);
+	}
+
+
+};
+
+
+
+int minDistance()
+{
+	// Initialize min value
+	int min = numeric_limits<int>::max();
+	int min_index;
+
+	for (int i = 0; i < all_nodes.size(); i++) {
+		if (all_nodes[i]->visited == false && all_nodes[i]->distance <= min) {
+			min = all_nodes[i]->distance;
+			min_index = i;
+		}
+
+	}
+
+	return min_index;
+}
+
+
+
+void constructGraph(Node *n) {
+
+	array<int, 4> new_state;
+	for (int i = 0; i < n_bucks; i++) {
+		if (n->can_fill(i)) {
+			new_state = n->copyState();
+			new_state[i] = capacities[i];
+			n->addState(new_state);
+		}
+		
+		if (n->can_empty(i)) {
+			new_state = n->copyState();
+			new_state[i] = 0;
+			n->addState(new_state);
+		}
+
+		for (int j = 0; j < n_bucks; j++) {
+			if (i == j)
+				continue;
+			
+			int measure = n->can_pour(i, j);
+			if (measure > 0) {
+				new_state = n->copyState();
+				new_state[i] -= measure;
+				new_state[j] += measure;
+				n->addState(new_state);
+			}
+		}
+	}
+
+}
+
+int dijkstra()
+{
+	for (int count = 0; count < all_nodes.size(); count++)
+	{
+		int u = minDistance();
+		all_nodes[u]->visited == true;
+
+		if (all_nodes[u]->distance > minimumDistance)
+			continue;
+
+		if (all_nodes[u]->target && (all_nodes[u]->distance) < minimumDistance)
+			minimumDistance = (all_nodes[u]->distance);
+
+		for (int i = 0; i < all_nodes[u]->next.size(); i++) {
+			Node *v = all_nodes[u]->next[i];
+			if (v->visited == false && all_nodes[u]->distance != numeric_limits<int>::max() && all_nodes[u]->distance + 1 < v->distance)
+				v->distance = all_nodes[u]->distance + 1;
+		}
+	}
+
+	return -1;
+}
+
+int main() {
+
+	array<int, 4> initial_arr = { 0, 0, 0, 0 };
+	Node *initial = new Node(initial_arr);
+	initial->distance = 0;
+	all_states[initial_arr] = initial;
+	all_nodes.push_back(initial);
+
+	T = 4;
+	capacities = { 1, 2, 3, 5 };
+	constructGraph(initial);
+	minimumDistance = numeric_limits<int>::max();
+	dijkstra();
+	cout << minimumDistance;
+
+	cout << "!";
+
+	return 0;
+}
+
+
+/*
 const int max_cap = 25;
 const int n_bucks = 4;
 const int n = max_cap*max_cap;
@@ -23,6 +197,12 @@ bool hasTarget[n];
 bool visited[n];
 int dist[n];
 bool sptSet[n];
+
+
+struct Node{
+
+
+};
 
 int cap[n_bucks];
 
@@ -45,12 +225,6 @@ int minDistance(int V)
 	return min_index;
 }
 
-int gcd(int a, int b)
-{
-	if (b == 0)
-		return a;
-	return gcd(b, a%b);
-}
 
 
 
@@ -193,11 +367,7 @@ int main() {
 			cout << "-1\n";
 			continue;
 		}
-		/*
-		if(T % gcd(gcd(gcd(cap[0], cap[1]), cap[2]), cap[3]) != 0) {
-			cout << "-1\n";
-			continue;
-		}*/
+		
 
 		transition(state);
 		cout << dijkstra(0, latest_id) << "\n";
@@ -209,3 +379,4 @@ int main() {
 }
 
 
+*/
