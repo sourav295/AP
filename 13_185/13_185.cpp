2b0da5cd279a8dfe2eea3823c6e2597a5b0c105f
spanda2 <spanda2@SRH702-8S>
2017-05-03 12:40:55 +0800

    dcsdc

--- 13_185/13_185.cpp
+++ 13_185/13_185.cpp
@@ -22,70 +22,95 @@ const int n_lines = 5000;
 const int n_words_in_line = 200;
 const int n_dist_word = 5000;
 
-int max_word = -1;//'z' - 'a' + 1;
-map<string, bool> uniqueWords;
+int max_wordSize = -1;
 
-struct Word{
-	int w[n_words_in_line];
-	string text;
+vector<string> all_words;
+map<string, bool> unique_words;
 
-	Word(string x) {
-		text = x;
-		int n = x.length();
-		for (int i = 0; i < n; i++) {
-			w[i] = tolower(x[i]) - 'a';
-			text[i] = tolower(x[i]);
-		}
-		for (int i = n; i < n_words_in_line; i++) {
-			w[i] = max_word;
+char smallest_word = int('a') - 1;
+
+void countSort(int exp)
+{
+	string output[n_dist_word]; // output array
+	int count[27] = { 0 };
+	int i;
+
+	for (i = 0; i < all_words.size(); i++)
+		count[(all_words[i])[exp] - 'a' + 1]++;
+
+	for (i = 1; i < 27; i++)
+		count[i] += count[i - 1];
+
+	for (i = all_words.size() - 1; i >= 0; i--)
+	{
+		output[count[(all_words[i])[exp] - 'a' + 1] - 1] = (all_words[i]);
+		count[(all_words[i])[exp] - 'a' + 1]--;
+	}
+
+	for (i = 0; i < all_words.size(); i++)
+		all_words[i] = output[i];
+}
+
+void radixsort()
+{
+	for (int i = 0; i < all_words.size(); i++) {
+		for (int j = all_words[i].length();j < max_wordSize; j++) {
+			all_words[i].push_back(smallest_word);
 		}
 	}
-	Word() {}
-};
+
+	for (int exp = max_wordSize - 1; exp >= 0; exp--)
+		countSort(exp);
+
+	
+}
 
 
-vector<Word> allwords;
-int pos = 0;
-bool isAlphabeticallySmaller(const Word& x, const Word& y) { return x.w[pos]< y.w[pos]; }
 
 int main()
 {
 	string line;
 	string word;
-	int max_wordSize = 0;
+	
 	while (getline(cin, line)) {
-		
 		int i = 0;
 		while(i < line.length()) {
-			if (!isalnum(line[i])) {
-				i++;;
+			if (!isalpha(line[i])) {
+				i++;
 				continue;
 			}
 			int start = i;
 			int end = i;
-			while (isalnum(line[end])) {
+			while (isalpha(line[end])) {
+				line[end] = tolower(line[end]);
 				end++;
 			}
 			i = end;
 
 			word = line.substr(start, end - start);
-			if (max_wordSize < word.length())
-				max_wordSize = word.length();
+			if (unique_words.count(word) == 0) {
+				int len = word.length();
+				if (max_wordSize < len)
+					max_wordSize = len;
 
-			if (uniqueWords.find(word) == uniqueWords.end()) {
-				allwords.push_back(Word(word));
-				uniqueWords[word] = true;
+				unique_words[word] = true;
+				all_words.push_back(word);
 			}
+			
 		}
 		
 	}
+	radixsort();
 	
-	for (pos = max_wordSize - 1; pos >= 0; pos--)
-		sort(allwords.begin(), allwords.end(), isAlphabeticallySmaller);
-	
-	for (int i = 0; i < allwords.size(); i++)
-		cout << allwords[i].text << "\n";
-		//if(i == 0 || (i > 0 && allwords[i].text != allwords[i-1].text))
+	for (int i = 0; i < all_words.size(); i++) {
+		int j = 0;
+		while (j < all_words[i].length() && isalpha((all_words[i])[j])) {
+			cout << (all_words[i])[j];
+			j++;
+		}
+		cout << "\n";
+	}
+
 			
 
 
