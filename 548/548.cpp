79ebb991c482f2cfc49b0cc19184f738e7760b5b
spanda2 <spanda2@SRH702-8S>
2017-05-08 13:59:07 +0800

    dafa

--- 548/548.cpp
+++ 548/548.cpp
@@ -19,52 +19,13 @@ using namespace std;
 
 
 
-unsigned long long getSeqOfOne(unsigned long long x, unsigned long long y) {
-	
-	//unsigned long long x = pow(10, 18);
-	int closestHigherPower2 = floor(log(x) / log(2)) + 1;
-	unsigned long long lower_lim = pow(2, closestHigherPower2 - 1);
-	lower_lim--;
-	if (lower_lim >= y) {
-		unsigned long long res = (pow(2, closestHigherPower2));
-		res--;
-		return res;
-	}
-	else {
-		return 0;
-	}
-	
-}
-
-unsigned long long getHighestSum(unsigned long long x, unsigned long long y) {
-	unsigned long long bitsToChange, x_copy, sum;
-	x_copy = x;
-	int highest_hideFlip = -1;
-	bitsToChange = x ^ y;
-	for (int i = 0; bitsToChange != 0; bitsToChange >>= 1, x >>= 1, i++) {//shift towards left 
-		bool change = bitsToChange & 1;
-		bool hideFlip = change && (x & 1);
-		if (hideFlip) {
-			highest_hideFlip = i;
-		}	
-	}
-	
-	unsigned long long noChange_mask = 0;
-	if (highest_hideFlip > -1)
-			noChange_mask = (1 << (highest_hideFlip + 1)) - 1;
-	//cout << noChange_mask << "\n"; 
-	sum = (x_copy & noChange_mask) | (y & (~noChange_mask));
-	return sum;
-	
-}
-
 
 
 int main()
 {
 	
 	
-	unsigned long long a, b, candidate1, candidate2;
+	unsigned long long a, b, c, b_copy;
 	string line;
 	while (getline(cin , line)) {
 		stringstream ss(line);
@@ -75,16 +36,24 @@ int main()
 			continue;
 		}
 		
+		b_copy = b;
+
+		int furthest_mutilation = -1;
+		int count = 0;
+		while (b > 0) {
+			bool a_last = a & 1;
+			bool b_last = b & 1;
+			if (a_last != b_last)
+				furthest_mutilation = count;
+			count++;
+			
+			a = a >> 1;
+			b = b >> 1;
+		}
 		
-		candidate1 = 0;
-		candidate2 = 0;
-		
-		candidate1 = getSeqOfOne(b, a);
-		candidate2 = getHighestSum(b-1, b);
-		if (candidate1 > candidate2)
-			cout << setprecision(40)<<candidate1 << "\n";
-		else
-			cout << setprecision(40)<<candidate2 << "\n";
+		c = (1 << (furthest_mutilation + 1)) - 1;
+
+		cout << (b_copy|c) <<"\n";
 
 	}
 	
