29a0a56566640fe1601ca235346448fa23c023c4
spanda2 <spanda2@SRH702-8S>
2017-05-17 18:14:06 +0800

    s

--- 542/542.cpp
+++ 542/542.cpp
@@ -1,68 +1,132 @@
-// 278.cpp : Defines the entry point for the console application.
-//
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <math.h>
 #include <iostream>
 #include <vector>
-#include <string>
-#include <set>
+#include <utility>
+#include <queue> 
+#include<string>
+#include <stack>
+#include <limits>
+#include <sstream>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <stdlib.h>
 using namespace std;
 
+const int n_limit = 100;
 
-vector<int> acceptable_cycles_for_range(int cycle_time, int start_time, int end_time) {
+struct Signal {
 
-	vector<int> results;
-	
-	int cn_max = floor(start_time / (2 * cycle_time));
-	int cn_min = ceil((end_time + 5 - cycle_time) / (2 * cycle_time));
+	int id;
+	int time_period;
 
-	for (int i = cn_min; i <= cn_max; i++)
-		results.push_back(i);
+	Signal(int i, int tp ) {
+		id = i;
+		time_period = tp;
+	}
 
-	return results;
+	pair<bool, int> isGreen(int time) {
+
+		int upperLim = time / (2 * time_period);//remove numbers after decimal
+		float lowerLim = (float)(time - time_period + 5) / (2 * time_period);
+
+		return{ lowerLim < upperLim, upperLim};
+	}
+
+	pair<int, int> getTimeBound(int i) {
+
+		float lower = 2 * i * time_period;
+		float upper = lower + time_period - 5 - 1;
+
+		return { lower, upper };
+
+	}
+
+
+	Signal() {}
+};
+
+int compareSignals(const void * a, const void * b)
+{
+	return ((*(Signal*)a).time_period - (*(Signal*)b).time_period);
+}
+
+Signal signals[n_limit];
+
+bool inInterval(pair<int, int> &bounds, Signal newSignal) {
+
+	pair<bool, int> lower_boundCmp = newSignal.isGreen(bounds.first);
+	pair<bool, int> upper_boundCmp = newSignal.isGreen(bounds.second);
+
+	bool isIntersecting = (lower_boundCmp.first || upper_boundCmp.first);
+	if (isIntersecting) {
+
+
+
+
+	}
+	else
+		return false;
 
 }
 
 
-
-int execute(vector<int> cycleTime_queue) {
-
-	int reference_cycleTime = cycleTime_queue.back();
-	cycleTime_queue.pop_back();
-	//choose a reference signal and check if it is valid for all
-	for (int i = 1; i < 5; i++) {
-
-		vector<int> common_cycles;
-
-		int validTimeRange_start = reference_cycleTime * i * 2;//exclusive
-		int validTimeRange_end   = validTimeRange_start + (reference_cycleTime - 5);//inclusive
-	
-		for (int j = 0; j < cycleTime_queue.size(); j++) {
-			vector<int> acceptable = acceptable_cycles_for_range(cycleTime_queue[j], validTimeRange_start, validTimeRange_end);
-			set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), std::back_inserter(common_cycles));
-
-		}
+void execute(int n) {
+	//ref is the first signal
+	int ref = 0;
+	float ref_lim = (float)5 * 60 * 60 / (2 * signals[ref].time_period);
 
 
-
-		cout << "\n" << validTimeRange_start << " " << validTimeRange_end << "--" << cycle_range.size();
+	for (int i = 1; i <= ref_lim; i++) {
+		signals[ref].getTimeBound(i);
 
 
 	}
 
-	return 0;
 }
 
+bool hasThreeZeros(string line) {
+	stringstream ss(line);
+	int a, b, c;
+	ss >> a;
+	ss >> b;
+	ss >> c;
+	return a == 0 && b == 0 && c == 0;
 
+}
 
 int main()
 {
-	vector<int> cycleTime_queue;
-	cycleTime_queue.push_back(19);
-	cycleTime_queue.push_back(20);
+	string line;
+	int count = 0;
+	while (1) {
 
-	execute(cycleTime_queue);
+		getline(cin, line);
+
+		if (hasThreeZeros(line)) {
+			break;
+		}
+
+
+		stringstream ss(line);
+
+		int input;
+		while (ss >> input) {
+			if (input == 0) {
+				qsort(signals, count, sizeof(Signal), compareSignals);
+				execute(count);
+				count = 0;
+			}
+			else {
+				signals[count++] = Signal(count, input);
+			}
+
+		}
+
+
+	}
 
 	return 0;
 }
